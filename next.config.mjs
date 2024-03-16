d8b8084e158a41b29fc246248f396af43de6f534
Raphael Corrêa <raphaeldeoliveiracorrea@gmail.com>
2024-03-16 01:43:44 -0300

    chore: enable autofix to unused imports in biome, fix test-utils import, enable autofix biome issues in vscode, remove export mode in next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,16 @@
-import bundleAnalyzer from '@next/bundle-analyzer';
+import bundleAnalyzer from "@next/bundle-analyzer";
 
 const withBundleAnalyzer = bundleAnalyzer({
-  enabled: process.env.ANALYZE === 'true',
+	enabled: process.env.ANALYZE === "true",
 });
 
 export default withBundleAnalyzer({
-  output: 'export',
-  reactStrictMode: false,
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  experimental: {
-    optimizePackageImports: ['@mantine/core', '@mantine/hooks'],
-  },
+	// output: 'export',
+	reactStrictMode: false,
+	eslint: {
+		ignoreDuringBuilds: true,
+	},
+	experimental: {
+		optimizePackageImports: ["@mantine/core", "@mantine/hooks"],
+	},
 });
--- src/test-utils/render.tsx
+++ src/test-utils/render.tsx
@@ -1,6 +1,6 @@
 import { MantineProvider } from "@mantine/core";
 import { render as testingLibraryRender } from "@testing-library/react";
-import { theme } from "../src/theme";
+import { theme } from "../theme";
 
 export function render(ui: React.ReactNode) {
 	return testingLibraryRender(ui, {
