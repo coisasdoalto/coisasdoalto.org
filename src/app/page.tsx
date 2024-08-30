d42aa19154d3e2954da3fcd193b85c701927ce24
Raphael Corrêa <raphaeldeoliveiracorrea@gmail.com>
2024-08-30 14:25:56 -0300

    chore: add padding in home page in sm size

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
-import { Center, Divider, Stack, Title } from "@mantine/core";
+import { Center, Container, Divider, Stack, Title } from "@mantine/core";
+import { Fragment } from "react";
 import { Hero } from "~/components/Hero";
 import { LastArticlesItem } from "~/components/LastArticlesItem/LastArticlesItem";
 
@@ -19,41 +20,41 @@ export default function HomePage() {
 	return (
 		<>
 			<Hero />
-			<Center mt="lg">
-				<Stack>
-					<Title order={2}>Livros</Title>
+			<Container size="md">
+				<Center mt="lg">
+					<Stack>
+						<Title order={2}>Livros</Title>
 
-					<Stack gap={0}>
-						{allBooks.map((book) => (
-							<>
-								<LastArticlesItem
-									key={book.slug}
-									href={`/books/${book.slug}/${book.chapters[0].slug}`}
-									itemName={book.title}
-									author={book.author}
-								/>
-								<Divider my="sm" />
-							</>
-						))}
+						<Stack gap={0}>
+							{allBooks.map((book) => (
+								<Fragment key={book.slug}>
+									<LastArticlesItem
+										href={`/books/${book.slug}/${book.chapters[0].slug}`}
+										itemName={book.title}
+										author={book.author}
+									/>
+									<Divider my="sm" />
+								</Fragment>
+							))}
+						</Stack>
+
+						<Title order={2}>Últimos artigos adicionados</Title>
+
+						<Stack gap={0}>
+							{allPosts.map((post) => (
+								<Fragment key={post.slug}>
+									<LastArticlesItem
+										href={`/posts/${post.slug}`}
+										itemName={post.title}
+										author={post.author}
+									/>
+									<Divider my="sm" />
+								</Fragment>
+							))}
+						</Stack>
 					</Stack>
-
-					<Title order={2}>Últimos artigos adicionados</Title>
-
-					<Stack gap={0}>
-						{allPosts.map((post) => (
-							<>
-								<LastArticlesItem
-									key={post.slug}
-									href={`/posts/${post.slug}`}
-									itemName={post.title}
-									author={post.author}
-								/>
-								<Divider my="sm" />
-							</>
-						))}
-					</Stack>
-				</Stack>
-			</Center>
+				</Center>
+			</Container>
 		</>
 	);
 }
