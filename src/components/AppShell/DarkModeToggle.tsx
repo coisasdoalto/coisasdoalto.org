67486c8021972aee0620711d4884dd078208a9d7
Raphael Corrêa <raphaeldeoliveiracorrea@gmail.com>
2024-08-30 13:52:16 -0300

    chore: position dark mode button on the right from md size

--- src/components/AppShell/DarkModeToggle.tsx
+++ src/components/AppShell/DarkModeToggle.tsx
@@ -12,6 +12,7 @@ export function DarkModeToggle() {
 			variant="outline"
 			onClick={() => toggleColorScheme()}
 			title="Acender/desligar a luz"
+			ml="auto"
 		>
 			{dark ? <IconBulb size={18} color="white" /> : <IconBulbOff size={18} />}
 		</ActionIcon>
