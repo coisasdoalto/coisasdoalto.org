d6dbeffb20cf4d9adeac924eeada38272e404137
Karina Formentini <kaformentini@gmail.com>
2024-11-10 15:41:47 -0300

    Add warning regarding translation

--- src/components/BookPostView/index.tsx
+++ src/components/BookPostView/index.tsx
@@ -1,7 +1,8 @@
-import { Box, Container, Divider, Title } from "@mantine/core";
+import { Alert, Anchor, Box, Container, Divider, Title } from "@mantine/core";
 import markdownToHtml from "~/lib/markdownToHtml";
 import type { Post as PostType } from "~/types/post";
 
+import { IconInfoCircle } from "@tabler/icons-react";
 import { Article } from "../Article";
 
 type BookPostView = {
@@ -13,6 +14,21 @@ export async function BookPostView({ post }: BookPostView) {
 
 	return (
 		<Container pt="md">
+			<Alert
+				mb={20}
+				variant="light"
+				color="gray"
+				title="Tradução"
+				icon={<IconInfoCircle />}
+			>
+				Este texto foi traduzido com ferramentas de IA, e ainda não foi revisado
+				por um humano. Pode conter erros graves. Confira sempre o link do texto
+				original:{" "}
+				<Anchor size="sm" href={post.translatedFrom} target="_blank">
+					{post.translatedFrom}
+				</Anchor>
+			</Alert>
+
 			<Box id="book-post-view-header" component="header">
 				<Title order={1}>{post.title}</Title>
 				<Divider my="md" />
